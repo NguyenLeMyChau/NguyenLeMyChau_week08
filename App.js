31c8f129a6be50e67adcc62e25c761693381200a
My Chau <nguyenlemychau1201@gmail.com>
2023-11-16 20:02:26 +0700

    Create modal for add note in NoteScreen.js

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StyleSheet} from 'react-native';
 import StackNavigator from './navigation/StackNavigator';
 import AddNoteScreen from './Components/AddNoteScreen';
+import NoteScreen from './Components/NoteScreen';
 
 export default function App() {
   return (
     <StackNavigator/>
     //<AddNoteScreen/>
+
   );
 }
 
--- Components/NoteScreen.js
+++ Components/NoteScreen.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View, TextInput, Modal } from 'react-native';
+import moment from 'moment';
 
 
 
@@ -10,11 +11,36 @@ export default function NoteScreen({ navigation, route }) {
 
     var [dataUser, setDataUser] = useState([]);
 
+    var [show, setShow] = useState(false);
+
+    var [content, setContent] = useState('');
+    var timeNote = moment(Date(new Date())).format('DD-MM-YYYY');
 
     useEffect(() => {
         getAPINote(),
             getAPIUser()
     }, []);
+
+
+
+
+    const saveAPINote = async () => {
+
+        const url = "http://localhost:3000/note";
+        let result = await fetch(url, {
+            method: "POST",
+            headers: { 'Accept': 'application/json, text/plain, */*', "Content-Type": "application/json" },
+            body: JSON.stringify({ userId: userId.userId, content, timeNote })
+        });
+        result = await result.json();
+        if (result) {
+            getAPINote()
+            alert("Data is added");
+        } else {
+            alert("Error");
+        }
+    }
+
 
     const getAPINote = async () => {
         const url = "http://localhost:3000/note?userId=" + userId.userId;
@@ -77,15 +103,48 @@ export default function NoteScreen({ navigation, route }) {
             </View>
 
             <View style={styles.footer}>
-                <TouchableOpacity style={styles.btnRegister}
+                <TouchableOpacity style={styles.btnAddNote}
                     onPress={() => {
-                        navigation.navigate('AddNoteScreen', { userId: userId.userId })
+                        // navigation.navigate('AddNoteScreen', { userId: userId.userId })
+                        setShow(true);
 
                     }}
                 >
                     <Text style={{ fontSize: 25, fontWeight: 500, color: '#FFFFFF' }}>ADD NOTE</Text>
                 </TouchableOpacity>
             </View>
+
+
+            <Modal visible={show} transparent={true}>
+                <View style={{ width: '80%', height: 220, justifyContent: 'center', alignItems: 'center', marginTop: 200, marginLeft: 40, border: '1px solid #C4C4C4', backgroundColor: 'white' }}>
+                    <View style={{ width: '100%', height: 30, marginTop: '-20px' }}>
+                        <Text style={{ fontSize: 25, fontWeight: 500, color: '#8353E2', left: 30 }}>Add Note</Text>
+                    </View>
+
+                    <View style={{ width: '100%', height: 50, marginTop: 15 }}>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Content"
+                            value={content}
+                            onChangeText={(text) => setContent(text)}
+                        />
+
+                    </View>
+
+                    <View style={{ width: '100%', height: 50, marginTop: 15, flexDirection: 'row' }}>
+                        <TouchableOpacity style={styles.btnRegister} onPress={saveAPINote}>
+                            <Text style={{ fontSize: 25, fontWeight: 500, color: '#FFFFFF' }}>Send</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={styles.btnRegister} onPress={() => {
+                            setShow(false);
+                        }}>
+                            <Text style={{ fontSize: 25, fontWeight: 500, color: '#FFFFFF' }}>Close</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                </View>
+            </Modal>
 
         </View>
     );
@@ -118,18 +177,28 @@ const styles = StyleSheet.create({
     },
 
     input: {
-        width: '80%',
+        width: '90%',
         height: 50,
-        padding: 10,
         paddingLeft: 20,
         fontSize: 20,
-        marginTop: 30,
         borderRadius: 10,
-        border: '1px #9095A0 solid'
+        border: '1px #9095A0 solid',
+        marginLeft: 20,
 
     },
 
     btnRegister: {
+        width: '40%',
+        height: 50,
+        backgroundColor: '#8353E2',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        left: 20,
+        marginLeft: 10
+    },
+
+    btnAddNote: {
         width: '80%',
         height: 50,
         backgroundColor: '#8353E2',
